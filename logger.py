b08c65522ae51f7a64b311b744a8b1b2e3a99929
JoBe <jolego879@outlook.de>
2025-03-28 07:23:21 +0000

    Updated logger, i didnt use the most recent logger version ig

--- logger.py
+++ logger.py
@@ -1,6 +1,5 @@
 # Logger used by coral
 
-import time
 if __name__ == "__main__":
   raise RuntimeError("`logger.py` is not meant to be run unless imported")
 
@@ -19,7 +18,7 @@ def get_path() -> str:
       return get_path()
   except FileNotFoundError:
     return f"latest.log"
-  return f"log_{ct}.log"
+    return f"log_{ct}.log"
   
 class logger():
   
@@ -34,7 +33,7 @@ class logger():
     self.newEntry(f"Initiated file as {self.LOG_PATH}", "Info")
   
   def newEntry(self, msg: str, level: Literal["Debug", "Info", "Error", "Fatal"]="Debug") -> None:
-    """Log to existing log, or create a new on and log to that one if needed."""
+    """Log to existing log, or create a new one and log to that one if needed."""
     try:
       with open(self.LOG_PATH, "a", errors="strict") as file:
         file.write(f"{time.strftime("%H:%M:%S", time.localtime())} [{level}]: {msg}\n")
@@ -62,5 +61,20 @@ class logger():
   def extensiveError(self, obj) -> None:
     self.newEntry(f"{obj}, traceback to {obj.__traceback__.tb_frame}", "Error")
     self.newEntry(f"Traceback object at {obj.__traceback__}", "Error")
-    self.newEntry(f"Line: {obj.__traceback__.tb_lineno}", "Error")
+    self.newEntry(f"TB Lasti: {obj.__traceback__.tb_lasti}", "Error")
+    self.newEntry(f"TB Lineno: {obj.__traceback__.tb_lineno}", "Error")
+    self.newEntry(f"TB Next: {obj.__traceback__.tb_next}", "Error")
     self.newEntry(f"Exiting Program", "Fatal")
+    
+  def debug(self, msg: str) -> None:
+    self.newEntry(msg, level="Debug")
+    
+  def info(self, msg: str) -> None:
+    self.newEntry(msg, level="Info")
+    
+  def error(self, msg: str) -> None:
+    self.newEntry(msg, level="Error")
+    
+  def fatal(self, error: object, msg: str) -> None:
+    self.newEntry(f"{error}: {msg}", "Fatal")
+    raise error
