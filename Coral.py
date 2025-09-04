3a58d2d211fb4554e2716c16786613db6ace52ff
JoBe <165585785+JoBeGaming@users.noreply.github.com>
2025-09-04 10:24:11 +0200

    Update Coral.py

--- Coral.py
+++ Coral.py
@@ -13,24 +13,24 @@ from logger import *
 log = logger()
 
 while True:
-  inp = str(input("Please input a valid command: ")).lower()
-  log.debug(f"Got input(s): {inp}")
-  if inp.startswith("asm"):
-    entry = A_entry
-    inp.removeprefix("asm ")
-  elif inp.startswith("emu"): 
-    entry = E_entry
-    inp.removeprefix("asm")
-  else: 
-    log.error("Input is not recognized as valid input, trying again")
-    continue
+    inp = str(input("Please input a valid command: ")).lower()
+    log.debug(f"Got input(s): {inp}")
+    if inp.startswith("asm"):
+        entry = A_entry
+        inp.removeprefix("asm ")
+    elif inp.startswith("emu"): 
+        entry = E_entry
+        inp.removeprefix("asm")
+    else: 
+        log.error("Input is not recognized as valid input, trying again")
+        continue
   
-  FileName = f"{inp.split('.')[0]}.{FILE_EXTENSION}"
-  inp.removeprefix(f"{FileName} ")
-  log.debug(f"Input got converted to {inp}, with file extension {FILE_EXTENSION} being used")
+    file_name = f"{inp.split('.')[0]}.{FILE_EXTENSION}"
+    inp.removeprefix(f"{file_name} ")
+    log.debug(f"Input got converted to {inp}, with file extension {FILE_EXTENSION} being used")
   
-  Flags = inp
-  log.debug(f"Using {Flags}")
+    flags = inp
+    log.debug(f"Using {flags}")
 
-  entry(FileName, Flags)
-  break
+    entry(file_name, flags)
+    break
